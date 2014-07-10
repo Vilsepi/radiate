e514809a0d2d06e2411ec8a281af2fff98d8948e
Ville Seppanen <ville.t.seppanen@iki.fi>
2014-07-11 00:04:58 +0300

    Added upstart jobs to start service on boot

--- radiate-client/radiate-client.sh
+++ radiate-client/radiate-client.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
 # Prerequisites:
-# - chromium-browser, x11-xserver-utils, unclutter
+# - google-chrome (or chromium-browser), x11-xserver-utils, unclutter
 # - Run dpkg-reconfigure x11-common and allow all users to start X session
 #   or alternatively start the xinit with sudo
 #
 
 default_url="http://localhost:9000"
 resolution="1366,768"
-browser_path="/usr/bin/chromium-browser"
+browser_path="/usr/bin/google-chrome"
 
 if [ -z "$1" ]; then
     echo "URL parameter was not given, using default URL $url"
@@ -29,15 +29,5 @@ else
 fi
 
 # Start browser UI
-xinit $browser_path --kiosk --window-size=$resolution --app="$url" &
+xinit $browser_path --kiosk --disable-translate --window-size=$resolution --app="$url"
 
-# Give X server some time to start so other X-related commands would work
-sleep 1
-
-# Hide cursor
-unclutter -display localhost:0 -idle 10 &
-
-# Disable screensaver (requires package x11-xserver-utils)
-xset s off -display localhost:0.0
-xset -dpms -display localhost:0.0
-xset s noblank -display localhost:0.0

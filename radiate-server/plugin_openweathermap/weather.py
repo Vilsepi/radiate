fa5fc1b37b2e74c3be1eab32f4a4d7f7f8cd8302
Ville Seppanen <ville.t.seppanen@iki.fi>
2014-07-11 23:51:48 +0300

    Changed openweathermap plugin to show forecast instead of current weather

--- radiate-server/plugin_openweathermap/weather.py
+++ radiate-server/plugin_openweathermap/weather.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 # Specify cities here you wish to monitor
-CITIES = ['Tampere,FI']
+CITY = 'Tampere,FI'
 
 env = Environment(loader=PackageLoader(__name__, 'templates'))
 template = env.get_template('template.html')
@@ -23,7 +23,7 @@ template = env.get_template('template.html')
 def _get_weather_for_city(location):
 
     try:
-        url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric' % location
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&units=metric' % location
         result = requests.get(url)
         log.debug('From cache: %s' % result.from_cache)
 
@@ -39,6 +39,19 @@ def _get_weather_for_city(location):
 
     return weather_data
 
+# Filter data
+def _mangle_data(data):
+    # TODO wtf these timezones are so messed up in openweather
+    obj = {}
+    obj['time'] = str(datetime.fromtimestamp(data['dt']).strftime('%H:%M'))
+    obj['debug_time'] = data['dt_txt']
+    obj['icon'] = data['weather'][0]['icon']
+    obj['temp_min'] = data['main']['temp_min']
+    obj['temp_avg'] = (data['main']['temp_max'] + data['main']['temp_min'])/2
+    obj['temp_max'] = data['main']['temp_max']
+    obj['text'] = data['weather'][0]['main']
+    return obj
+
 
 # Return display-ready card in HTML
 def get_card():
@@ -47,16 +60,14 @@ def get_card():
         CACHE_PATH = "cache_weather"
         if __name__ == "plugin_openweathermap.weather":
             CACHE_PATH = "plugin_openweathermap/cache_weather"
-        requests_cache.install_cache(CACHE_PATH, backend='sqlite', expire_after=600)
+        requests_cache.install_cache(CACHE_PATH, backend='sqlite', expire_after=900)
 
-        weather_data = map(_get_weather_for_city, CITIES)
+        weather_data = _get_weather_for_city(CITY)
+	mangled_data = map(_mangle_data, weather_data['list'])
+	location = weather_data['city']['name']
 
-        # TODO: This currently shows the same info for all cities as the first city
-        updated_at = str(datetime.fromtimestamp(weather_data[0]['dt']).strftime('%H:%M'))
-        sunrise = str(datetime.fromtimestamp(weather_data[0]['sys']['sunrise']).strftime('%H:%M'))
-        sunset = str(datetime.fromtimestamp(weather_data[0]['sys']['sunset']).strftime('%H:%M'))
+        return template.render(location=location, data=mangled_data[:8])
 
-        return template.render(data=weather_data, updated_at=updated_at, sunrise=sunrise, sunset=sunset)
     except Exception, err:
         log.error(err)
         return "<h1>Card failed on top level</h1>"

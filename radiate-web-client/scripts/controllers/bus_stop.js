150992a6e7f7052fa3915f31501bbc15199b1498
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-23 21:49:26 +0200

    Added views for flat bus list and grouped by bus stop

--- radiate-web-client/scripts/controllers/bus_stop.js
+++ radiate-web-client/scripts/controllers/bus_stop.js
@@ -1,11 +1,6 @@
-'use strict';
-
-angular
-  .module('radiateApp', [
-  ]);
 
 angular.module('radiateApp')
-  .controller('BusController', ['$scope', '$http', function($scope, $http) {
+  .controller('BusStopCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.apiUrl = "http://localhost:5000/api/tkl_lissu?stops=3733,3523,3737";
     $scope.busStopData = null;

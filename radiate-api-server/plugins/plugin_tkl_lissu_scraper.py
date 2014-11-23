46dabfbff956a9b4a75648352cccac4991c570e2
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-23 23:25:44 +0200

    Calculate minute estimate for every bus

--- radiate-api-server/plugins/plugin_tkl_lissu_scraper.py
+++ radiate-api-server/plugins/plugin_tkl_lissu_scraper.py
@@ -2,7 +2,7 @@
 
 from yapsy.IPlugin import IPlugin
 from bs4 import BeautifulSoup
-from time import strftime
+import datetime
 import requests
 import requests_cache
 import json
@@ -21,6 +21,20 @@ class LissuScrape(IPlugin):
         requests_cache.install_cache(cache_path, backend='sqlite', expire_after=7200)
         log.debug("Installed cache")
 
+    def _get_minutes_until_time(self, time_string):
+        """ Takes a time as hh:mm string and calculates time left in minutes until time is reached """
+        # TODO time zones and summer time :(
+
+        now = datetime.datetime.now()
+        time_string_split = time_string.split(':')
+        upcoming = now.replace(hour=int(time_string_split[0]), minute=int(time_string_split[1]), second=0, microsecond=0)
+
+        if upcoming < now:
+            upcoming += datetime.timedelta(days=1)
+        time_left = upcoming - now
+
+        return time_left.seconds/60
+
     def _enrich_estimate(self, time_string):
         """ Unify Lissu times which can be in '13 min', '07:33' or '07:33z' format"""
         
@@ -30,17 +44,19 @@ class LissuScrape(IPlugin):
             if time_schedule:
                 time_object['is_tracked'] = False
                 time_object['time'] = time_schedule.group(0)
-            time_estimate = re.search('(\d+) min', time_string)
-            if time_estimate:
-                time_object['is_tracked'] = True
-                time_object['estimate_minutes'] = time_estimate.group(1)
+                time_object['estimate_minutes'] = self._get_minutes_until_time(time_schedule.group(0))
+            else:
+                time_estimate = re.search('(\d+) min', time_string)
+                if time_estimate:
+                    time_object['is_tracked'] = True
+                    time_object['estimate_minutes'] = time_estimate.group(1)
         except IndexError:
             log.error("Failed to parse times")
 
         return time_object
 
     def _scrape_html_into_json(self, html_string):
-        try:
+        #try:
             scraped_soup = BeautifulSoup(html_string)
             bus_stop_info = scraped_soup.tr.find_all("td")
             bus_stop_name = bus_stop_info[0].text[:-7]
@@ -55,8 +71,8 @@ class LissuScrape(IPlugin):
                 line_list.append({'line': keys[0].text, 'destination': keys[2].text, 'eta': map(self._enrich_estimate, [keys[3].text, keys[4].text])})
 
             return {'bus_stop_name': bus_stop_name, 'updated_at': updated_at, 'next_buses': line_list}
-        except Exception, err:
-            return json.dumps({"status":"err","message":"Failed to scrape source html"})
+        #except Exception, err:
+        #    return json.dumps({"status":"err","message":"Failed to scrape source html"})
 
     def _get_data_for_bus_stop(self, stop_id):
 

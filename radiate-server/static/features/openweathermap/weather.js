0cad11ed2720da6f704aaeba8da128c37ec3257a
Ville Seppanen <ville.seppanen@gofore.com>
2015-10-12 21:19:34 +0300

    Added weather plot with chartist

--- radiate-server/static/features/openweathermap/weather.js
+++ radiate-server/static/features/openweathermap/weather.js
@@ -1,11 +1,29 @@
 
 angular.module('radiateApp')
-  .controller('OpenWeatherCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+  .controller('OpenWeatherCtrl', ['$scope', '$http', '$interval', '$filter', function($scope, $http, $interval, $filter) {
     var apiUrl = "/api/openweathermap";
 
     $scope.getWeatherData = function(){
       $http.get(apiUrl).success(function(data){
         $scope.weatherData = data;
+
+        var timestamps = _.map(data.forecast.list, function (item) {
+          return $filter('date')(item.dt*1000, 'HH');
+        });
+        var temperatures = _.map(data.forecast.list, function (item) {
+          return item.main.temp;
+        });
+
+        new Chartist.Line('.ct-chart', {
+          labels: timestamps,
+          series: [temperatures]
+        }, {
+          fullWidth: true,
+          chartPadding: {
+            right: 20
+          }
+        });
+
       });
     };
 

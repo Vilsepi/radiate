9ca2b1c6d306512aac848dc2c4abc2a7e77c39d9
Ville Seppanen <ville.seppanen@gofore.com>
2015-10-15 19:59:06 +0300

    Filter uninteresting bus lines. To be moved to backend.

--- radiate-server/static/features/tkl_lissu_scraper/bus_stop.js
+++ radiate-server/static/features/tkl_lissu_scraper/bus_stop.js
@@ -2,10 +2,17 @@
 angular.module('radiateApp')
   .controller('BusStopCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
     var apiUrl = "/api/tkl_lissu?stops=3733,3523,3737";
+    var busLineBlacklist = ["38", "65"];
 
     $scope.getBusStopData = function(){
       $http.get(apiUrl).success(function(data){
-        $scope.busStopData = data["stops"];
+        $scope.busStopData = _.map(data["stops"], function(stop){
+          stop.next_buses = _.filter(stop.next_buses, function(line){
+            return !_.contains(busLineBlacklist, line.line);
+          });
+          return stop;
+        });
+
       });
     };
 

860cc9739d941310bb61fce6be2112c7707437b1
Ville Seppanen <ville.seppanen@gofore.com>
2016-01-13 19:35:16 +0200

    Filter uninteresting bus lines separately for each bus stop

--- radiate-server/static/features/tkl_lissu_scraper/bus_stop.js
+++ radiate-server/static/features/tkl_lissu_scraper/bus_stop.js
@@ -2,16 +2,22 @@
 angular.module('radiateApp')
   .controller('BusStopCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
 
-    var apiUrl = "/api/tkl_lissu?stops=3733,3523,3737";
+    var apiUrl = "/api/tkl_lissu?stops=3733,3523,3642,3737";
     //var apiUrl = "/static/test/bus.json";
 
-    var busLineBlacklist = ["38", "65"];
+    // List of bus lines to filter out for each bus stop
+    var busLineBlacklist = {
+      '3733': ['38', '65'], // Poliisikoulu
+      '3523': ['5'], // Hervantakeskus pohjoiseen
+      '3642': ['3', '32'], // Hervantakeskus etelään
+      '3737': ['13', '38'] // Tuulanhovi
+    };
 
     $scope.getBusStopData = function(){
       $http.get(apiUrl).success(function(data){
         $scope.busStopData = _.map(data["stops"], function(stop){
           stop.next_buses = _.filter(stop.next_buses, function(line){
-            return !_.contains(busLineBlacklist, line.line);
+            return !_.contains(busLineBlacklist[stop.bus_stop_id], line.line);
           });
           return stop;
         });
